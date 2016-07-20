e7d86d2ed34633e65620a02694c8223055c1d5e9
Justin <Crank50@icloud.com>
2016-07-20 10:08:16 -0600

    Warnings are methods uncalled by a class or by designating a main

---
+++ ToolBox/Collection/ArrayListExample.java
@@ -0,0 +1,27 @@
+package ToolBox.Collection;
+
+/**
+ * Created by Justin on 7/20/16.
+ */
+public class ArrayListExample {
+
+    // Create a new ArrayList named name that holds Strings
+    //TODO: ArrayList here
+
+    // add another String to the ArrayList
+    public void addElement(String name) {
+        //TODO: logic here
+    }
+
+    // get a String from the ArrayList at a specific index
+    public String getElement(int index) {
+        //TODO: logic here
+        return ""; // DELETE ME
+    }
+
+    // print out all the Strings in the ArrayList
+    public void printAllElements() {
+        //TODO: logic here
+    }
+
+}
