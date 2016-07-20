d9bcccd52187ce99133c3c09ae32c553eb94dd97
Justin <Crank50@icloud.com>
2016-07-20 10:46:08 -0600

    Warnings are methods uncalled by a class or by designating a main

--- ToolBox/Collection/ArrayListExample.java
+++ ToolBox/Collection/ArrayListExample.java
@@ -1,27 +1,51 @@
 package ToolBox.Collection;
 
+import java.util.ArrayList;
+
 /**
  * Created by Justin on 7/20/16.
  */
 public class ArrayListExample {
 
+
     // Create a new ArrayList named name that holds Strings
-    //TODO: ArrayList here
+    ArrayList<String> names = new ArrayList<>();
+
+    public static void main(String[] args) {
+        ArrayListExample arrayListExample = new ArrayListExample();
+        arrayListExample.addElement("bob");
+        arrayListExample.addElement("lex");
+        arrayListExample.addElement("farsey");
+        arrayListExample.addElement("tank");
+        arrayListExample.addElement("sally");
+        System.out.println("element at 4: " + arrayListExample.getElement(4));
+        arrayListExample.printAllElements();
+
+
+    }
 
     // add another String to the ArrayList
     public void addElement(String name) {
-        //TODO: logic here
+        names.add(name);
     }
 
     // get a String from the ArrayList at a specific index
     public String getElement(int index) {
-        //TODO: logic here
-        return ""; // DELETE ME
+        return names.get(index);
+
     }
 
     // print out all the Strings in the ArrayList
     public void printAllElements() {
-        //TODO: logic here
+
+        for (String name : names) {
+            System.out.println(name);
+
+            //  System.out.println(names);
+            // System.out.println("-------------toString------------");
+            // System.out.println(names.toString());
+
+        }
     }
 
 }
